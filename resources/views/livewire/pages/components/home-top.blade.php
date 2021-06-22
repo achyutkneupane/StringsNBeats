7195fdac89acb0de44cd276f05d0c30abdf1f575
Achyut Neupane <aneupane@kumberland.com>
2021-06-22 14:57:44 +0545

    Img Alt updated

--- resources/views/livewire/pages/components/home-top.blade.php
+++ resources/views/livewire/pages/components/home-top.blade.php
@@ -5,7 +5,7 @@
                 <div class="carousel-inner">
                     @foreach($featured as $article)
                     <a class="carousel-item{{ $loop->first ? ' active' : '' }}" href="{{ route('viewArticle',$article->slug) }}">
-                        <img class="d-block w-100" src="{{ asset('storage/'.$article->featured_image) }}" style="opacity: 0.3" alt="Third slide" height="400">
+                        <img class="d-block w-100" src="{{ asset('storage/'.$article->featured_image) }}" style="opacity: 0.3" alt="{{ $article->title }} - {{ config('app.name') }}" height="400">
                         <div class="carousel-caption">
                             <div class="text-dark text-uppercase h2">{{ $article->title }}</div>
                         </div>
@@ -29,7 +29,7 @@
                     @if($latests->count() > 0)
                     @foreach($latests as $latest)
                     <a href="{{ route('viewArticle',$latest->slug) }}" class="flex-row my-1 d-flex col-md-12 ml-0 row">
-                        <div class="col-4 row px-0" style="width:100%;"><img src="{{ asset('storage/'.$latest->featured_image) }}" style="height:100px;" class="col-12 px-0"></div>
+                        <div class="col-4 row px-0" style="width:100%;"><img src="{{ asset('storage/'.$latest->featured_image) }}" alt="{{ $latest->title }} - {{ config('app.name') }}" style="height:100px;" class="col-12 px-0"></div>
                         <div class="pl-4 col-8 d-flex flex-column justify-content-center">
                             <h6>{{ $latest->title }}</h6>
                         </div>
--- resources/views/livewire/pages/components/home/post-component.blade.php
+++ resources/views/livewire/pages/components/home/post-component.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('viewArticle',$article->slug) }}">
     <div class="my-1 d-flex flex-column">
-        <div style="object-fit: cover; overflow: hidden; width:100%; height:250px;"><img src="{{ asset('storage/'.$article->featured_image) }}" style="width:100%; height: 100%;"></div>
+        <div style="object-fit: cover; overflow: hidden; width:100%; height:250px;"><img src="{{ asset('storage/'.$article->featured_image) }}" alt="{{ $article->title }} - {{ config('app.name') }}" style="width:100%; height: 100%;"></div>
         <div class="pt-3 text-center">
             <h5>{{ $article->title }}</h5>
         </div>
