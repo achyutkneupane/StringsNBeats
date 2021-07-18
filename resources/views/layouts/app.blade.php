e4d593b3f4dafe0ea2ee2757d3b2f726d27b44c4
Achyut Neupane <aneupane@kumberland.com>
2021-07-19 02:42:05 +0545

    Facebook pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,24 @@
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-HS0XEPQE5K"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '366677928135938');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=366677928135938&ev=PageView&noscript=1"
+    />
+    </noscript>
+  <!-- End Facebook Pixel Code -->
     <script data-ad-client="ca-pub-2094944997068259" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <meta name="facebook-domain-verification" content="y1p8q4hk3fojgo2p1gncr2bwx43lyb" />
 </head>
