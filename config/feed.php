c481f4c05feb4f3a3b0d9c1621b46244a92eec54
Achyut Neupane <aneupane@kumberland.com>
2021-08-12 17:21:27 +0545

    With international

--- config/feed.php
+++ config/feed.php
@@ -92,5 +92,35 @@ return [
              */
             'type' => 'application/atom+xml',
         ],
+        'international' => [
+            /*
+             * Here you can specify which class and method will return
+             * the items that should appear in the feed. For example:
+             * 'App\Model@getAllFeedItems'
+             *
+             * You can also pass an argument to that method:
+             * ['App\Model@getAllFeedItems', 'argument']
+             */
+            'items' => 'App\Models\Article@getFeedInternational',
+
+            /*
+             * The feed will be available on this url.
+             */
+            'url' => '/feed/internationals',
+
+            'title' => 'Strings N\' Beats International Feed',
+            'description' => 'RSS Feed for International coverages in  Strings N\' Beats.',
+            'language' => 'en-US',
+
+            /*
+             * The view that will render the feed.
+             */
+            'view' => 'feed::atom',
+
+            /*
+             * The type to be used in the <link> tag
+             */
+            'type' => 'application/atom+xml',
+        ],
     ],
 ];
