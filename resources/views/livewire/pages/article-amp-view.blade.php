9946eff35ed4b8552aef11aa253b8199aea16082
achyutkneupane <aneupane@kumberland.com>
2021-08-20 02:37:44 +0545

    Ad Removed

--- resources/views/livewire/pages/article-amp-view.blade.php
+++ resources/views/livewire/pages/article-amp-view.blade.php
@@ -29,12 +29,12 @@
     <script async src="https://cdn.ampproject.org/v0.js"></script>
     <script async custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"></script>
     <script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>
+    <script async custom-element="amp-facebook" src="https://cdn.ampproject.org/v0/amp-facebook-0.1.js"></script>
     <script async custom-element="amp-list" src="https://cdn.ampproject.org/v0/amp-list-0.1.js"></script>
     <script async custom-template="amp-mustache" src="https://cdn.ampproject.org/v0/amp-mustache-0.2.js"></script>
     <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
     <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
     <script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
-    <script async custom-element="amp-skimlinks" src="https://cdn.ampproject.org/v0/amp-skimlinks-0.1.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width">
     <meta name="author" content="Strings N' Beats">
@@ -228,6 +228,5 @@
             }
         </script>
     </amp-analytics>
-    <amp-skimlinks layout="nodisplay" publisher-code="197401X1675341"></amp-skimlinks>
 </body>
 </html>
