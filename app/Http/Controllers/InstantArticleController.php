6c01288cf8ebf85c4d02090b53f57895389c2741
achyutkneupane <aneupane@kumberland.com>
2021-09-17 15:38:45 +0545

    IA figures added

--- app/Http/Controllers/InstantArticleController.php
+++ app/Http/Controllers/InstantArticleController.php
@@ -25,7 +25,6 @@ class InstantArticleController extends Controller
             $iframe = preg_replace('/<iframe(.+?)>/m','<figure class="op-interactive"><iframe$1>',$img);
             $article->content = preg_replace('/<\/iframe>/m',"</iframe></figure>",$iframe);
         });
-        dd($articles);
         return response()->view('ia', compact('articles','title'))->header('Content-Type', 'application/xml');
     }
 }
